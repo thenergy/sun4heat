1d71e684b57deb640e77886b0044653abc3c4fa3
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2021-12-09 10:04:42 -0300

    prueba

--- server/run_scripts/mapa_emisiones.sh
+++ server/run_scripts/mapa_emisiones.sh
@@ -7,4 +7,5 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
 #bokeh serve mapa_emisiones --port 5509
 #bokeh serve mapa_emisiones --port 5509 --allow-websocket-origin=18.222.135.159:5509 &
-bokeh serve mapa_emisiones -- port 5609 --show
+#bokeh serve mapa_emisiones -- port 5609 --show
+bokeh serve mapa_emisiones --allow-websocket-origin=18.222.135.159:5509 &
