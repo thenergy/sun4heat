7fbd1d57e991f6b303a60dfcb88f4ea47cdad531
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2021-12-09 10:08:24 -0300

    prueba

--- server/run_scripts/eval_proy.sh
+++ server/run_scripts/eval_proy.sh
@@ -2,6 +2,6 @@
 
 #source activate bokeh2
 cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
-#bokeh serve eval_proy --port 5510 --allow-websocket-origin=18.222.135.159:5510 &
+bokeh serve eval_proy --port 5510 --allow-websocket-origin=18.222.135.159:5510 &
 #bokeh serve eval_proy --port 5610 --show
-bokeh serve eval_proy --allow-websocket-origin=18.222.135.159:5510 &
+#bokeh serve eval_proy --allow-websocket-origin=18.222.135.159:5510 &
