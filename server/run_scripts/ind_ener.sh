d287c8ecf5e022e501d7a11c6cc404901903daee
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2021-12-09 15:22:54 -0300

    Prueba

--- server/run_scripts/ind_ener.sh
+++ server/run_scripts/ind_ener.sh
@@ -5,6 +5,6 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 
 #conda activate bokeh2
 cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
-#bokeh serve ind_ener --port 5512 --allow-websocket-origin=18.216.78.202:5512 &
+bokeh serve ind_ener --port 5512 --allow-websocket-origin=18.216.78.202:5512 &
 #bokeh serve ind_ener --port 5612 --show
 #bokeh serve ind_ener --allow-websocket-origin=18.222.135.159:5512 &
--- server/run_scripts/swh_calc.sh
+++ server/run_scripts/swh_calc.sh
@@ -5,6 +5,6 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 
 #conda activate bokeh2
 cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
-#bokeh serve swh_calc --port 5511 --allow-websocket-origin=18.216.78.202:5511 &
+bokeh serve swh_calc --port 5511 --allow-websocket-origin=18.216.78.202:5511 &
 #bokeh serve swh_calc --port 5611 --show
 #bokeh serve swh_calc --allow-websocket-origin=18.222.135.159:5511 &
