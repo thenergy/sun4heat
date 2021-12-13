82f69be6d1542e339b161e3db684c7a15a04433e
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2021-12-13 15:21:24 -0300

    prueba arreglo

--- server/run_scripts/mapa_emisiones.sh
+++ server/run_scripts/mapa_emisiones.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 CONDA_BASE=$(conda info --base)
-#source $CONDA_BASE/etc/profile.d/conda.sh
+source $CONDA_BASE/etc/profile.d/conda.sh
 
 #conda activate bokeh2
 cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
 #bokeh serve mapa_emisiones --port 5509
-bokeh serve mapa_emisiones --port 5012 --allow-websocket-origin=18.216.118.176:5012 &
+bokeh serve mapa_emisiones --port 5021 --allow-websocket-origin=18.216.118.176:5021 &
 #bokeh serve mapa_emisiones -- port 5609 --show
 #bokeh serve mapa_emisiones --allow-websocket-origin=18.222.135.159:5509 &
