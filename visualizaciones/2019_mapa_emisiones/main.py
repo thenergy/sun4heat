ab0d78eb3afc7895613a2412e7930efd1cd47ccf
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2021-12-16 12:37:37 -0300

    pruebaaa

--- visualizaciones/2019_mapa_emisiones/main.py
+++ visualizaciones/2019_mapa_emisiones/main.py
@@ -931,7 +931,7 @@ layout = column(
     row(maxEmpr, multi_choice),
     row(dropdownRegion, latNorte, latSur),
     row(dropDownTiles, dropDownCat),
-    row(buttCalcUpdate, buttExportCSV_Excel, buttdownload),
+    row(buttCalcUpdate, buttExportCSV_Excel, buttondownload),
     Spacer(height=spc - 20),
     row(p1, data_table),
     Spacer(height=spc + 30),
