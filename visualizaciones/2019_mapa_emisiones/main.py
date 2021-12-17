d9fa20fc855de45b4148949762a06a0d73b74f74
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2021-12-17 12:55:08 -0300

    arreglo

--- visualizaciones/2019_mapa_emisiones/main.py
+++ visualizaciones/2019_mapa_emisiones/main.py
@@ -159,8 +159,8 @@ def ReadIndus():
     # indus = pd.read_csv(path + 'datos/RETC/ckan_ruea_2019_v1.csv', names=header, encoding="latin-1",skiprows=1,sep=';',decimal=',')
     indus = pd.read_excel(
         path + "datos/RETC/indus_ll.xlsx",
-        names=header,
-        encoding="utf-8-sig"
+        names=header
+        # encoding="utf-8-sig"
     )
 
     indus.ton_emision = pd.to_numeric(indus.ton_emision, errors="coerce")
