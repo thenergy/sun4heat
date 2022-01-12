f1236dbd5fefc052359cd16344da1c8396cc6ca3
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2022-01-12 12:48:18 -0300

    arreglo directorios

--- server/run_scripts/mapa_emisiones.sh
+++ server/run_scripts/mapa_emisiones.sh
@@ -6,6 +6,6 @@ source /home/ubuntu/anaconda3/etc/profile.d/conda.sh
 #conda activate bokeh2
 cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
 #bokeh serve mapa_emisiones --port 5509
-bokeh serve mapa_emisiones --port 5509 --allow-websocket-origin=18.216.118.176:5509 &
+bokeh serve 2019_mapa_emisiones --port 5333 --allow-websocket-origin=18.117.130.153:5333 &
 #bokeh serve mapa_emisiones -- port 5609 --show
 #bokeh serve mapa_emisiones --allow-websocket-origin=18.222.135.159:5509 &
--- server/run_scripts/swh_calc.sh
+++ server/run_scripts/swh_calc.sh
@@ -5,6 +5,6 @@ CONDA_BASE=$(conda info --base)
 
 #conda activate bokeh2
 cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
-bokeh serve swh_calc --port 5014 --allow-websocket-origin=18.216.118.176:5014 &
+bokeh serve swh_calc --port 5014 --allow-websocket-origin=18.117.130.153:5014 &
 #bokeh serve swh_calc --port 5611 --show
 #bokeh serve swh_calc --allow-websocket-origin=18.222.135.159:5511 &
