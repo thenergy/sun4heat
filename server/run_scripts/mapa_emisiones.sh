ba896a308fbb2c64fe47a72c1b73889f2c88de34
DiegoNaranjoCL <diego.naranjo@thenergy.com>
2022-05-20 14:13:58 -0400

    arreglo servers

--- server/run_scripts/mapa_emisiones.sh
+++ server/run_scripts/mapa_emisiones.sh
@@ -8,8 +8,8 @@ cd /home/ubuntu/Thenergy/diego/sun4heat/visualizaciones
 
 #bokeh serve mapa_emisiones --port 5509
 
-bokeh serve 2019_mapa_emisiones --port 6666 --allow-websocket-origin=18.117.130.153:6666 &
-bokeh serve 2020_mapa_emisiones --port 7777 --allow-websocket-origin=18.117.130.153:7777 &
+bokeh serve 2019_mapa_emisiones --port 6776 --allow-websocket-origin=18.117.130.153:6776 &
+bokeh serve 2020_mapa_emisiones --port 7667 --allow-websocket-origin=18.117.130.153:7667 &
 
 #bokeh serve mapa_emisiones -- port 5609 --show
 #bokeh serve mapa_emisiones --allow-websocket-origin=18.222.135.159:5509 &
